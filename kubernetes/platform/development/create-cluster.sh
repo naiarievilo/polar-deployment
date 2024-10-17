1412f607ed0c69c827438fd21b53c09c6390e70c
Ian Oliveira <naiarievilo@gmail.com>
2024-10-17 14:07:25 +0000

    End of chapter 9

---
+++ kubernetes/platform/development/create-cluster.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "\n📦 Initializing Kubernetes cluster...\n"
+
+minikube addons enable ingress --profile polar
+
+sleep 15
+
+echo "\n📦 Deploying platform services..."
+
+kubectl apply -f services
+
+sleep 5
+
+echo "\n⌛ Waiting for PostgreSQL to be deployed..."
+
+while [ $(kubectl get pod -l db=polar-postgres | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for PostgreSQL to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=db=polar-postgres \
+  --timeout=180s
+
+echo "\n⌛ Waiting for Redis to be deployed..."
+
+while [ $(kubectl get pod -l db=polar-redis | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Redis to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=db=polar-redis \
+  --timeout=180s
+
+echo "\n⛵ Happy Sailing!\n"
